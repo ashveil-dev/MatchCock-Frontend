80c30d6cbd394a30794792a3c4b1936bcaa7363b
ashveil-dev <ashveil-dev@github.com>
2025-10-31 14:37:15 +0900

    @Design 메인 페이지 문구 변경

--- src/App.tsx
+++ src/App.tsx
@@ -24,32 +24,35 @@ function App() {
             <img src="https://stauter-web-dev.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FImg_main.66654e82.png&w=256&q=100" alt="아이콘" />
           </div>
           <div id="textWrap">
-            <p className="w-3xs md:w-full px-6 md:px-0 text-center md:text-start mb-6 lg:mb-4 font-Pretendard text-xl md:text-3xl leading- font-bold"> 
+            <p className="w-2xs md:w-full break-all px-6 md:px-0 text-center md:text-start mb-6 lg:mb-4 font-Pretendard text-xl md:text-3xl leading- font-bold"> 
               <span className="bg-clip-text bg-linear-to-r from-BlushPink to-MysticIndigo text-transparent">
-                IT 스타터
+                배드민턴{' '}
               </span>
-              를 위한 가이드 <span className="hidden md:inline">:</span> 스타우터
+              대진표를 원하는대로 만들고 싶다고요?
             </p>
-            <div className="w-2xs md:w-xl mb-4 text-center md:text-start text-sm md:text-base leading-10 font-Pretendard font-light">
-              <p><b className="font-bold" >아웃소싱 제안에 필요한 문서</b>를 간편하게 정리하기!</p>
-              스타우터 테스트를 통해 요구사항 정의서부터 기능구조도, 
-              예시 화면까지 무료로 받아보세요!
+            <div className="w-2xs md:w-xl break-all mb-4 text-center md:text-start text-sm md:text-base leading-10 font-Pretendard font-light">
+              <p>🏸 경기에서 볼 여러 팀의 <b className="font-bold" >대진표를 모아보기!</b></p>
+              <p>❤️ <b className="font-bold" >원하는 클럽</b> 대진표를 한번에 모아놓고 살펴봐요</p>
+              <p>
+                😊 각 경기를 일일히 검색하지 않고 <b className="font-bold">한눈에 파악할 수 있어서 편리해요</b>
+              </p>
+
             </div>
           </div>
           <div id="buttonWrap" className="w-full">
-            <button className="w-full font-Pretendard font-bold text-white bg-RoyalAmethyst py-4 rounded-xl mb-4 leading-6">
+            <button className="w-full  font-Pretendard font-bold text-white bg-RoyalAmethyst py-4 rounded-xl mb-4 leading-6">
               지금 바로 시작하기
             </button>
           </div>
           <div id="detail" className="w-full text-[rgba(0,0,0,.65)] font-Pretendard font-light text-xs">
             <table className="w-full border-separate border-spacing border-spacing-4 md:border-spacing-6">
               <tr>
-                <td className="">예상 소요시간</td>
-                <td>약 8분 ~ 12분</td>
+                <td>사용 API</td>
+                <td>스포넷</td>
               </tr>
               <tr>
-                <td>추천 이용자</td>
-                <td>초기 기획을 완료한 IT 서비스 제작자</td>
+                <td className="">예상 소요시간</td>
+                <td>약 8분 ~ 12분</td>
               </tr>
             </table>
           </div>
