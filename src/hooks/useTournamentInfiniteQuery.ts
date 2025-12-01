90727152466192eee5873f412b5de9d6c638739c
ashveil-dev <ashveil.dev@gmail.com>
2025-12-02 02:21:41 +0900

    @Refactir Tournament의 useInfiniteQuery 분리하기

---
+++ src/hooks/useTournamentInfiniteQuery.ts
@@ -0,0 +1,49 @@
+import { fetchTournamentList } from "@apis";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
+
+interface IUseTournamentInfiniteQuery {
+    type: "page" | "infinite",
+    search: string,
+    stateFilter: string[],
+    dateFilter: {
+        from?: Date,
+        to?: Date
+    } | undefined,
+    order: {
+        [key: string]: "asc" | "desc"
+    }
+}
+
+export default function useTournamentInfiniteQuery({
+    type, search,
+    stateFilter, dateFilter,
+    order
+}: IUseTournamentInfiniteQuery) {
+    const stableInfiniteQueryParams = useMemo(() => ({
+        type, search,
+        stateFilter, dateFilter,
+        order
+    }), [
+        type, search,
+        JSON.stringify(stateFilter),
+        JSON.stringify(dateFilter),
+        JSON.stringify(order),
+    ]);
+
+    const { isLoading: isInfiniteLoading, isFetching: isInfiniteFetching,
+         data: infiniteData, fetchNextPage, hasNextPage } = useInfiniteQuery({
+        queryKey: [
+            "tournamentList",
+            stableInfiniteQueryParams
+        ],
+        queryFn: ({ pageParam = 0 }) => fetchTournamentList({ ...stableInfiniteQueryParams, cursor: pageParam }),
+        initialPageParam: 0,
+        getNextPageParam: (lastPage) => {
+            return lastPage.data?.nextCursor
+        },
+        enabled: type === "infinite"
+    })
+
+    return { isInfiniteLoading, isInfiniteFetching, infiniteData, fetchNextPage, hasNextPage}
+}
